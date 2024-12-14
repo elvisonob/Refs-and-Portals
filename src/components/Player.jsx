5d19485e818d13287de32639e211a43f271f5c83
elvisonob <elvisonob@yahoo.com>
2024-12-14 10:10:25 +0000

    Adding Ref to control inputs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+  //using Ref now
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  const playerName = useRef();
+
+  const onSubmitName = () => {
+    setEnteredPlayerName(playerName.current.value);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={onSubmitName}>Set Name</button>
       </p>
     </section>
   );

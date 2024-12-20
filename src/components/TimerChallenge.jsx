900be70f86fe754df97aea69b5702e652caf3d1f
elvisonob <elvisonob@yahoo.com>
2024-12-20 04:37:11 +0000

    Implementing useRef

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,25 +1,32 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const TimerChallenge = ({ title, targetTime }) => {
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStart, setTimerStart] = useState(false);
 
+  const timer = useRef();
+
   const handleStart = () => {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
 
     setTimerStart(true);
   };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
+  };
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
       {timerExpired && <p>You lost</p>}
       <p className="challenge-time">
-        {targetTime} second {targetTime > 1 ? 's' : ''}
+        {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStart ? handleStop : handleStart}>
           {timerStart ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
